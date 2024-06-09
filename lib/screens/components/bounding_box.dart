ce0146a7b54889920abb960bc2f48149e6ff6f4f
Hardik Khurana <120127800+KhuranaHardik@users.noreply.github.com>
2024-06-09 15:46:31 +0530

    changes

---
+++ lib/screens/components/bounding_box.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+// The Selection box that comes at the box.
+class BoundingBox extends StatelessWidget {
+  final List<dynamic> results;
+  final int previewH;
+  final int previewW;
+  final double screenH;
+  final double screenW;
+  final String model;
+
+  const BoundingBox(this.results, this.previewH, this.previewW, this.screenH,
+      this.screenW, this.model,
+      {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> renderBoxes() {
+      return results.map((mapResult) {
+        var x0 = mapResult["rect"]["x"];
+        var w0 = mapResult["rect"]["w"];
+        var y0 = mapResult["rect"]["y"];
+        var h0 = mapResult["rect"]["h"];
+        double scaleW, scaleH, x, y, w, h;
+
+        if (screenH / screenW > previewH / previewW) {
+          scaleW = screenH / previewH * previewW;
+          scaleH = screenH;
+          var difW = (scaleW - screenW) / scaleW;
+          x = (x0 - difW / 2) * scaleW;
+          w = w0 * scaleW;
+          if (x0 < difW / 2) w -= (difW / 2 - x0) * scaleW;
+          y = y0 * scaleH;
+          h = h0 * scaleH;
+        } else {
+          scaleH = screenW / previewW * previewH;
+          scaleW = screenW;
+          var difH = (scaleH - screenH) / scaleH;
+          x = x0 * scaleW;
+          w = w0 * scaleW;
+          y = (y0 - difH / 2) * scaleH;
+          h = h0 * scaleH;
+          if (y0 < difH / 2) h -= (difH / 2 - y0) * scaleH;
+        }
+
+        return Positioned(
+          left: math.max(0, x),
+          top: math.max(0, y),
+          width: w,
+          height: h,
+          child: Container(
+            padding: const EdgeInsets.only(top: 5.0, left: 5.0),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: const Color.fromRGBO(37, 213, 253, 1.0),
+                width: 3.0,
+              ),
+            ),
+            child: Text(
+              "${mapResult["detectedClass"]} ${(mapResult["confidenceInClass"] * 100).toStringAsFixed(0)}%",
+              style: const TextStyle(
+                color: Color.fromRGBO(37, 213, 253, 1.0),
+                fontSize: 14.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        );
+      }).toList();
+    }
+
+    return Stack(
+      children: renderBoxes(),
+    );
+  }
+}
